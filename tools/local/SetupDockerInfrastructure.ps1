bd8d4ab88ccc3163d87dc861f4150832565535e6
Martin Slot <mas@impact.dk>
2019-11-03 19:03:01 +0100

    Added dynamic username, password and database name

--- tools/local/SetupDockerInfrastructure.ps1
+++ tools/local/SetupDockerInfrastructure.ps1
@@ -1,3 +1,21 @@
+param (
+    [string]$database_username = $( Read-Host "Database user (press enter for default sa)"),
+    [string]$database_name = $( Read-Host "Database name (press enter for default Bl0g)"),
+    [Security.SecureString]$database_password = $( Read-Host -AsSecureString "Database password" )
+ )
+
+ if(!$database_username)
+ {
+     $database_username="sa";
+ }
+ if(!$database_name) 
+ {
+     $database_name="Bl0g";
+ }
+ 
+ # I know this is bad practice but my powershell Foo isn't that good, and I don't what else to do
+ $database_password_decrypted = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($database_password))
+
 # this creates a network that is used when spinning up individual containers when testing in Visual Studio.
 # projects interacting with fx storage, should have this
 #
@@ -11,7 +29,7 @@
 # 
 # and remember to point it to the network (as it is done above)
 
-docker network create bl0g_network
+# docker network create bl0g_network
 
 # Default storage system used by azure functions.
 # To read more about it, visit: https://docs.microsoft.com/en-us/azure/storage/common/storage-use-azurite
@@ -20,3 +38,11 @@ docker network create bl0g_network
 # docker run -p 10000:10000 -p 10001:10001 --network test --name azure_storage mcr.microsoft.com/azure-storage/azurite azurite --blobHost 0.0.0.0 --queueHost 0.0.0.0 
 #
 # This creates a container named azure_storage for other azure functions to use. Remember to start it after system reboot.
+
+# Now we want to run start all docker containers locally.
+# First we run the docker-compose up.
+
+ $env:DATABASE_PASSWORD=$database_password_decrypted;$env:DATABASE_USER=$database_username;$env:DATABASE_NAME=$database_name;docker-compose up
+
+# Be aware of the fact that MSSQL takes a bit to start, so for the connectionstring for the migrations, configure it with a long timeout (30 sec default).
+# Be aware that the connection string for migrations is a connectionstring without the database name in it. This is appended to the connectionstring after the creation of the database.
